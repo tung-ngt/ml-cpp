5e3fceb606711c4d57a32e844e167674ae775912
Nguyen Thanh Tung <tung.ngt@outlook.com>
2023-04-24 00:37:28 +0700

    finish linear regresion

--- main.cpp
+++ main.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 #include "Matrix/Matrix.h"
-#include "Matrix/MatrixExceptions.h"
+#include "LinearRegresion/LinearRegresion.h"
 
 using namespace std;
 
 int main()
-{
-    Matrix A{
-        {2, 0},
-        {0, 2},
-    };
-    Matrix B {
-        {0, 1},
-        {1, 0}
-    };
-    (A + B).inverse().show(10, 2);
+{       
+    LinearRegresion model(1);
+   
+    Matrix X {{147, 150, 153, 158, 163, 165, 168, 170, 173, 175, 178, 180, 183}};
+    X = X.T();
+    Matrix Y{{49, 50, 51, 54, 58, 59, 60, 62, 63, 64, 66, 67, 68}};
+    Y = Y.T();
+
+    model.train(13, X, Y);
+    cout << model.predict(Matrix{{155}}) << endl;
+    Matrix W = model.W();
+    cout << (W.at(0, 0) + W.at(1, 0) * 155);
     return 0;
 }
